acb3633cf6758bf8b1328b3d5d2bfc13e6b651fc
mohamed beltagy <moh.bell@crowdbotics.com>
2021-09-12 20:25:17 +0200

    add users apps

--- notify_me/settings.py
+++ notify_me/settings.py
@@ -37,7 +37,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    ### third party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',#new
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    #'allauth.socialaccount.providers.google',
+    #'allauth.socialaccount.providers.facebook',
+    'django_extensions',
+
+    ## local apps ###
+    'users',
+
 ]
+######### authentications #############33
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
